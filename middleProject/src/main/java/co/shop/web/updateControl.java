1d9d1800d2193feede6b63de227559363c6c20fe
admin <admin@YD4-19>
2022-05-19 12:33:17 +0900

    20220519

--- middleProject/src/main/java/co/shop/web/updateControl.java
+++ middleProject/src/main/java/co/shop/web/updateControl.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import co.shop.service.ShopService;
 import co.shop.vo.ShopVO;
@@ -13,20 +14,27 @@ public class updateControl implements Controller {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			
-		String email = request.getParameter("email");
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/json; charset=utf-8");
+		response.setCharacterEncoding("utf-8");
+
+		HttpSession session = request.getSession();
+		String email = (String) session.getAttribute("email");
+
+		ShopService service = new ShopService();
+		ShopVO info = service.search(email);
+
+		request.setAttribute("info", info);
+		
+
 		String pw = request.getParameter("pw");
+		String pwcheck = request.getParameter("pwcheck");
 		String gender = request.getParameter("gender");
 		String jumin = request.getParameter("jumin");
 		String address = request.getParameter("address");
+		String acces = request.getParameter("acces");
 		String phone = request.getParameter("phone");
-
-		if (email.isEmpty() || pw.isBlank() || gender.isBlank() || jumin.isBlank()
-				|| address.isBlank() ||  phone.isBlank()) {
-			request.setAttribute("error", "모든 항목을 입력하세요 !");
-			request.getRequestDispatcher("shopView/update.jsp").forward(request, response);
-			return;
-		}
+		String loginway = request.getParameter("loginway");
 
 		ShopVO vo = new ShopVO();
 		vo.setEmail(email);
@@ -34,16 +42,35 @@ public class updateControl implements Controller {
 		vo.setGender(gender);
 		vo.setJumin(jumin);
 		vo.setAddress(address);
+		vo.setAcces(acces);
 		vo.setPhone(phone);
+		vo.setLoginway(loginway);
 
-		ShopService service = new ShopService();
-		service.update(vo);
-		
-		
+
+		ShopService service1 = new ShopService();
+		service1.update(vo);
+
 		request.setAttribute("email", email);
+//		if (pw.equals(pwcheck)) {
+//			// 전화번호길이
+//			if (phone.length() == 11) {
+//				request.getRequestDispatcher("userUpdate.jsp").forward(request, response);
+//
+//			} else {
+//				String error = "전화번호를 다시 입력해주세요.";
+//				request.setAttribute("error", error);
+//				request.getRequestDispatcher("userinfo/update.jsp").forward(request, response);
+//			}
+//		} else if (!pw.equals(pwcheck)) {
+//			String error = "비밀번호가 일치하지 않습니다.";
+//
+//			request.setAttribute("error", error);
+//			request.getRequestDispatcher("userinfo/update.jsp").forward(request, response);
+//
+//		}
 		
-		request.getRequestDispatcher("shopView/update.jsp").forward(request, response);
-
+		request.getRequestDispatcher("userUpdate.jsp").forward(request, response);
+		
 	}
 
 }
--- middleProject/src/main/webapp/WEB-INF/userinfo/list.jsp
+++ middleProject/src/main/webapp/WEB-INF/userinfo/list.jsp
@@ -14,7 +14,7 @@
 			<p>비밀번호 : ${info.pw }	</p>
 			<p>성별 : ${info.gender }</p>
 			<p>주소 : ${info.address }</p>
-			<p>주민번호 : ${info.jumin }</p>	
+			<p>생년월일 : ${info.jumin }</p>	
 			<p>전화번호 : ${info.phone }</p>
 
 </form>
